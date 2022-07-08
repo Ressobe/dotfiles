670bdaca0d0bf2737e326c45f8abf59ecbff5728
Ressobe <bartosz.sobina@tutanota.com>
2022-07-08 20:13:53 +0200

    Add picom with blur

--- x11/.dwm/autostart.sh
+++ x11/.dwm/autostart.sh
@@ -1,8 +1,8 @@
 wal -R &
+picom --experimental-backends &
 slstatus &
 dunst &
-picom --experimental-backends &
 sudo xbacklight -set 20 &
 xset r rate 300 50 &
 xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Accel Speed" 0.1 &
-emacs --daemon &
+# emacs --daemon &
