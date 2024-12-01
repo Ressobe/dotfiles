48ec37ae018c68437e11fe08cb29fe9d4dc73b2d
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-12-01 21:15:26 +0100

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "Vs Code Dark+ (Gogh)"
+config.font_size = 12
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 15,
+	right = 15,
+	top = 15,
+	bottom = 0,
+}
+
+return config
