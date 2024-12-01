751abcb3b745e5961c6cb8f1e1f74440858c4a4a
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-12-01 21:10:43 +0100

    add neovim config

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       transparent_background = true,
+--     }
+--   end,
+-- }
+
+return {
+  "Mofiqul/vscode.nvim",
+  name = "vscode",
+  priority = 1000,
+  opts = function()
+    return {
+      transparent_background = true,
+    }
+  end,
+}
---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+}
